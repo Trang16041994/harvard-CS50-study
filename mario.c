f0193b7180b1145f571faebaea39458085e58731
Trang16041994 <Trang16041994@users.noreply.github.com>
2022-02-04 11:28:40 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 0;
+    while (number < 1 || number > 8)
+    {
+        number = get_int("what number do you want? \n");
+    }
+    int socham = number - 1;
+    int sothang = 1;
+    // DI THEO CHIEU DOC
+    for (int j = 0; j < number; j++)
+    {
+        //DI THEO CHIEU NGANG
+        for (int i = 0; i < socham; i++)
+        {
+            printf (".");
+        }
+        for (int k = 0; k < sothang; k++)
+        {
+            printf("#");
+        }
+        printf("..");
+
+        for (int a = 0; a < sothang; a++)
+        {
+            printf("#");
+        }
+        sothang = sothang + 1;
+        printf("\n");
+        socham = socham - 1;
+    }
+}
